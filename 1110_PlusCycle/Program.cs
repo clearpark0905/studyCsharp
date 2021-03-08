1bbbfde122655c6f738c66268c4a636c5a223bf5
sanghyeok <psh2138@gmail.com>
2021-03-08 23:35:01 +0900

    algorithm 문제 풀이

---
+++ 1110_PlusCycle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num = int.Parse(Console.ReadLine());
+        int temp = 100;
+        int prevNum = num;
+        int i = 0;
+        while(num != temp)
+        {
+            i++;
+            temp = (prevNum%10)*10 + ((prevNum/10) + (prevNum%10))%10;
+            prevNum = temp;
+        }
+        Console.WriteLine(i);
+    }
+}
