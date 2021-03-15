66992ddfc7dbc9c390d1ec2f5345122a5c060025
sanghyeok <psh2138@gmail.com>
2021-03-15 22:58:10 +0900

    algorithm 문제풀이

---
+++ 1546_average/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        string[] sungjuk = Console.ReadLine().Split(' ');
+        float[] sungjukFloat = Array.ConvertAll(sungjuk, float.Parse);
+
+        float maxScore = float.MinValue;
+        foreach(float f in sungjukFloat)
+        {
+            if(maxScore < f)
+            {
+                maxScore = f;
+            }
+        }
+
+        float sum = 0;
+        for(int i=0; i<sungjukFloat.Length; i++)
+        {
+            sum += (sungjukFloat[i] /maxScore) * 100;
+        }
+
+        Console.WriteLine(sum / n);
+        
+    }
+}
