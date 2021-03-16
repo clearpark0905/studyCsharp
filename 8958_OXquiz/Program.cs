bc2c78706130b935d7809a0d446cf1ffe6f0d833
sanghyeok <psh2138@gmail.com>
2021-03-16 22:37:50 +0900

    algorithm 문제풀이

---
+++ 8958_OXquiz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8958_OXquiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] TestCase = new string[n];
+            for(int i=0; i<n; i++)
+            {
+                string temp;
+                int cnt = 0;
+                int score = 0;
+                TestCase[i] = Console.ReadLine();
+                for(int j=0; j<TestCase[i].Length;j++)
+                {
+                    temp = TestCase[i].Substring(j, 1);
+                    if(temp.Equals("O"))
+                    {
+                        score += ++cnt;
+                    } 
+                    else
+                    {
+                        cnt = 0;
+                    }
+                    
+                }
+                Console.WriteLine(score);
+            }
+
+        }
+    }
+}
---
+++ Al_4344_AboveAvg/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Al_4344_AboveAvg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<n; i++)
+            {
+                string[] line = Console.ReadLine().Split(' ');
+                int students = int.Parse(line[0]);
+                int[] studentScore = new int[students];
+                int sum = 0;
+                for(int j=0; j<students; j++)
+                {
+                    studentScore[j] = int.Parse(line[j+1]);
+                    sum += studentScore[j];
+                }
+                int aboveAvgStudent = 0;
+                for (int j = 0; j < students; j++)
+                {
+                    if(studentScore[j] > sum/students)
+                    {
+                        aboveAvgStudent++;
+                    }
+                }
+                double a = Math.Round((double)100 * aboveAvgStudent / students, 3);
+                sb.AppendLine(String.Format("{0:0.000}%", a));
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
